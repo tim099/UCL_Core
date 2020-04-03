0d8d125f826989986deb8cee302e29eab82ddb19
tim1125 <tim11251994@gmail.com>
2020-04-04 00:20:15 +0800

    Add Singleton

---
+++ UCL_Core_Scripts/UCL_Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL {
+    public class UCL_Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+        static T _instance;
+        static bool m_Destroyed = false;
+        public static T Instance {
+            get {
+                if(m_Destroyed) return null;
+
+                if(_instance == null) {
+                    GameObject singleton = new GameObject(typeof(T).Name+ "(AutoGen_UCL_Singleton)");
+                    _instance = singleton.AddComponent<T>();
+                    DontDestroyOnLoad(singleton);
+                }
+
+                return _instance;
+            }
+            set {
+                _instance = value;
+                _instance.name = typeof(T).Name + "(UCL_Singleton)";
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+        }
+        private void OnDestroy() {
+            m_Destroyed = true;
+            _instance = null;
+        }
+
+    }
+}
+

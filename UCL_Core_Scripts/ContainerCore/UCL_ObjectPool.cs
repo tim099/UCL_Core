3c293f97a56dcc6da2043dafe7af7f8a0afcb773
tim099 <tim11251994@gmail.com>
2020-04-16 16:11:03 +0800

    Add UCL_ObjectPool.cs Add UCL_Vector.cs

---
+++ UCL_Core_Scripts/ContainerCore/UCL_ObjectPool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL.Core.Container {
+
+    public class ObjectPool<T> where T : new() {
+        Queue<T> m_Pool = new Queue<T>();
+
+        public T Create() {
+            T target = default;
+            if(m_Pool.Count > 0) {
+                target = m_Pool.Dequeue();
+            } else {
+                target = new T();
+            }
+            return target;
+        }
+        public void Delete(T target) {
+            m_Pool.Enqueue(target);
+        }
+    }
+}

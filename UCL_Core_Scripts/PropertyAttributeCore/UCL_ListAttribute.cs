a73aadd496a243a58cd376630b28a06f79676860
tim099 <tim11251994@gmail.com>
2020-05-25 17:38:52 +0800

    Update UCL_ListAttribute.cs(Support accessor)

--- UCL_Core_Scripts/PropertyAttributeCore/UCL_ListAttribute.cs
+++ UCL_Core_Scripts/PropertyAttributeCore/UCL_ListAttribute.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System;
+using System.Reflection;
 
 namespace UCL.Core.PA {
     public class UCL_StrListAttribute : PropertyAttribute {
@@ -19,8 +20,33 @@ namespace UCL.Core.PA {
                     Debug.LogError("UCL_ListProperty method.Invoke Exception:" + e.ToString());
                 }
 
-            } else {
-                Debug.LogError("UCL_ListProperty:"+ type.Name+ ",func_name == null :" + func_name);
+            } else { //might be accessor
+                PropertyInfo propInfo = type.GetProperty(func_name);
+                if(propInfo == null) { // not accessor!!
+                    Debug.LogError("UCL_ListProperty:" + type.Name + ",func_name == null :" + func_name);
+                    return;
+                }
+                MethodInfo[] methInfos = propInfo.GetAccessors();
+                for(int ctr = 0; ctr < methInfos.Length; ctr++) {
+                    MethodInfo m = methInfos[ctr];
+                    //Console.WriteLine("Accessor #{0}:", ctr + 1);
+                    //Console.WriteLine("   Name: {0}", m.Name);
+                    //Console.WriteLine("   Visibility: {0}", GetVisibility(m));
+                    //Console.Write("   Property Type: ");
+                    // Determine if this is the property getter or setter.
+                    if(m.ReturnType == typeof(void)) {//setter
+                        //Console.WriteLine("Setter");
+                        //Console.WriteLine("   Setting the property value.");
+                        //  Set the value of the property.
+                        //m.Invoke(test, new object[] { "The Modified Caption" });
+                    } else {//getter
+                        //Console.WriteLine("Getter");
+                        // Get the value of the property.
+                        //Console.WriteLine("   Property Value: {0}", m.Invoke(test, new object[] { }));
+                        m_List = m.Invoke(null , new object[] { }) as string[];
+                        if(m_List != null) break;
+                    }
+                }
             }
         }
     }

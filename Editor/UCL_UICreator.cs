a0679ea9f209add0aef3c2f594c67999cc1afba8
tim099 <tim11251994@gmail.com>
2020-09-30 17:45:37 +0800

    Add UCL_Button.cs

---
+++ Editor/UCL_UICreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+namespace UCL.Core.UI.Editor {
+    public static class UCL_UICreator {
+        [UnityEditor.MenuItem("GameObject/UI/UCL_Button")]
+        private static void CreateUCL_Button() {
+            Object selectedObject = UnityEditor.Selection.activeObject;
+            if(selectedObject != null) {
+                Debug.LogWarning("CreateUCL_Button() selectedObject:" + selectedObject.name);
+            }
+
+            GameObject obj = selectedObject as GameObject;
+            Transform p = null;
+            if(obj != null) {
+                p = obj.transform;   
+            }
+            var but = UCL.Core.GameObjectLib.Create<UCL_Button>("UCL_Button", p);
+            UnityEditor.Selection.activeObject = but;
+            but.gameObject.AddComponent<Image>();
+        }
+    }
+}
---
+++ UCL_Core_Scripts/UICore/UCL_Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UCL.Core.UI {
+    public class UCL_Button : MonoBehaviour, IPointerDownHandler, IPointerExitHandler, IPointerUpHandler, IPointerEnterHandler {
+        public bool m_Pressed = false;
+        public bool m_Enter = false;
+
+        public void OnPointerEnter(PointerEventData eventData) {
+            m_Enter = true;
+            StateUpdate();
+        }
+        public void OnPointerExit(PointerEventData eventData) {
+            m_Enter = false;
+            StateUpdate();
+        }
+
+        public void OnPointerDown(PointerEventData eventData) {
+            m_Pressed = true;
+            StateUpdate();
+        }
+        public void OnPointerUp(PointerEventData eventData) {
+            m_Pressed = false;
+            StateUpdate();
+        }
+        int m_Times = 0;
+        virtual protected void StateUpdate() {
+            Debug.LogWarning(++m_Times+"m_Pressed:" + m_Pressed + ",m_Enter:" + m_Enter);
+        }
+    }
+}

3bb311c237f38ae867b53be956d8a2beb2dca67f
tim1125 <tim11251994@gmail.com>
2020-11-01 14:28:59 +0800

    Add UCL_Draggable.cs Add UCL_OnPointerEvent.cs

---
+++ UCL_Core_Scripts/UICore/UCL_Draggable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UCL.Core.UI
+{
+    public class UCL_Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+    {
+        public bool IsDragging { get { return m_Dragging; } }
+        protected Vector3 m_DragStartPosition = Vector3.zero;
+        protected bool m_Dragging = false;
+
+        #region drag
+
+        virtual public void OnDrag(PointerEventData eventData) {
+            var canvas = transform.GetComponentInParent<Canvas>();
+            Vector3 drag_pos = eventData.position.ToVec3() + m_DragStartPosition;
+            if(canvas.renderMode == RenderMode.ScreenSpaceCamera) {
+                drag_pos = canvas.worldCamera.ScreenToWorldPoint(drag_pos);
+            }
+            transform.position = drag_pos;
+            m_Dragging = true;
+        }
+        virtual public void OnBeginDrag(PointerEventData eventData) {
+            var canvas = transform.GetComponentInParent<Canvas>();
+            Vector3 drag_pos = transform.position;
+            if(canvas.renderMode == RenderMode.ScreenSpaceCamera) {
+                drag_pos = canvas.worldCamera.WorldToScreenPoint(drag_pos);
+            }
+            m_DragStartPosition = drag_pos - eventData.position.ToVec3();
+
+            m_Dragging = true;
+        }
+        virtual public void OnEndDrag(PointerEventData eventData) {
+            m_Dragging = false;
+        }
+        #endregion
+    }
+}
---
+++ UCL_Core_Scripts/UICore/UCL_OnPointerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UCL.Core.UI
+{
+    public class UCL_OnPointerEvent : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
+    {
+        public UCL.Core.UCL_Event m_OnPointerEnter = null;
+        public UCL.Core.UCL_Event m_OnPointerExit = null;
+        virtual public void OnPointerEnter(PointerEventData eventData) {
+            m_OnPointerEnter.UCL_Invoke();
+        }
+        virtual public void OnPointerExit(PointerEventData eventData) {
+            m_OnPointerExit.UCL_Invoke();
+        }
+    }
+}

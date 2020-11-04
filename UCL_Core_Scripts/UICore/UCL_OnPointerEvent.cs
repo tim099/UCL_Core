1b4657d007c8543f5c558c4d40c196c87fe3e34b
tim1125 <tim11251994@gmail.com>
2020-11-04 08:55:32 +0800

    Update UCL_OnPointerEvent.cs

--- UCL_Core_Scripts/UICore/UCL_OnPointerEvent.cs
+++ UCL_Core_Scripts/UICore/UCL_OnPointerEvent.cs
@@ -9,11 +9,14 @@ namespace UCL.Core.UI
     {
         public UCL.Core.UCL_Event m_OnPointerEnter = null;
         public UCL.Core.UCL_Event m_OnPointerExit = null;
+        public bool m_Enter = false;
         virtual public void OnPointerEnter(PointerEventData eventData) {
             m_OnPointerEnter.UCL_Invoke();
+            m_Enter = true;
         }
         virtual public void OnPointerExit(PointerEventData eventData) {
             m_OnPointerExit.UCL_Invoke();
+            m_Enter = false;
         }
     }
 }

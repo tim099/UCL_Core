74258b722116323f5b9b9ec9caf042faa9aafdff
tim099 <tim11251994@gmail.com>
2020-12-28 17:37:32 +0800

    Add UCL_DebugInspectorDemo

---
+++ UCL_CoreDemo/UCL_Core_DemoScripts/UCL_DebugInspectorDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL.Core.Demo {
+    [Core.ATTR.EnableUCLEditor]
+    public class UCL_DebugInspectorDemo : MonoBehaviour {
+        private void Start() {
+            Show();
+        }
+        [UCL.Core.ATTR.UCL_FunctionButton]
+        public void Show() {
+            UCL_DebugInspector.Show();
+        }
+
+        [UCL.Core.ATTR.UCL_FunctionButton]
+        public void Hide() {
+            UCL_DebugInspector.Hide();
+        }
+    }
+}
--- UCL_Core_Scripts/DebugCore/UCL_DebugInspector.cs
+++ UCL_Core_Scripts/DebugCore/UCL_DebugInspector.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 namespace UCL.Core {
     public class UCL_DebugInspector : MonoBehaviour {
-        private void OnEnable() {
-            
+        public static UCL_DebugInspector ins = null;
+        protected Rect m_WindowRect;
+
+        public static void Show() {
+            if(ins == null) {
+                ins = Core.GameObjectLib.Create<UCL_DebugInspector>("UCL_DebugInspector", null);
+            }
+            ins.ShowInspector();
         }
-        private void OnDisable() {
-            
+        public static void Hide() {
+            if(ins == null) return;
+            ins.HideInspector();
+        }
+
+
+        virtual public void ShowInspector() {
+            gameObject.SetActive(true);
+        }
+        virtual public void HideInspector() {
+            gameObject.SetActive(false);
+        }
+        virtual public void Init() {
+
+        }
+        private void OnGUI() {
+            const int edge = 5;
+            m_WindowRect = new Rect(edge, edge, Screen.width - 2 * edge, Screen.height - 2 * edge);
+            m_WindowRect = GUILayout.Window(132125, m_WindowRect, ConsoleWindow, "UCL DebugInspector");
+
+        }
+        virtual protected void ConsoleWindow(int id) {
+            GUILayout.BeginVertical();
+            var objs = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+            foreach(var obj in objs) {
+                if(GUILayout.Button(obj.name)) {
+
+                }
+            }
+            GUILayout.EndVertical();
         }
     }
 }

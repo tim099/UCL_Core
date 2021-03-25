f19d0b805319e522e3ce5f66f016faca065b6713
Tim Yeh <tim11251994@gmail.com>
2021-03-25 17:35:06 +0800

    Move EditorMappers into UCL_EditorMappers Folder

---
+++ UCL_Core_Scripts/CsvCore/UCL_CSVData.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL.Core.CsvLib {
+    [System.Serializable]
+    public class CSVRowData {
+        public int Count {
+            get {
+                if(m_Columes == null) return 0;
+                return m_Columes.Count;
+            }
+        }
+        public CSVRowData()
+        {
+
+        }
+        public CSVRowData(string iData) {
+            var columes = iData.Split(new[] { ',' });//, System.StringSplitOptions.RemoveEmptyEntries
+            for(int i = 0; i < columes.Length; i++) {
+                m_Columes.Add(columes[i]);
+            }
+        }
+        public void AddColume(string iValue)
+        {
+            m_Columes.Add(iValue);
+        }
+        public string Get(int iColume) {
+            if(iColume < 0 || iColume >= m_Columes.Count) return string.Empty;
+            return m_Columes[iColume];
+        }
+        public List<string> m_Columes = new List<string>();
+    }
+    [System.Serializable]
+    public class CSVData {
+        public int Count { get {
+                if(m_Rows == null) return 0;
+                return m_Rows.Count;
+            } }
+        public CSVData()
+        {
+
+        }
+        public CSVData(string iData) {
+            var rows = iData.SplitByLine();
+            for(int i = 0; i < rows.Length; i++) {
+                if(!string.IsNullOrEmpty(rows[i])) {
+                    m_Rows.Add(new CSVRowData(rows[i]));
+                }
+            }
+        }
+        public string ToCSV()
+        {
+            System.Text.StringBuilder aSB = new System.Text.StringBuilder();
+            for(int i = 0; i < m_Rows.Count; i++)
+            {
+                var aRow = m_Rows[i];
+                for(int j = 0; j < aRow.Count; j++)
+                {
+                    aSB.Append(aRow.Get(j));
+                    aSB.Append(',');
+                }
+                aSB.AppendLine();
+            }
+            return aSB.ToString();
+        }
+        public CSVRowData AddRow()
+        {
+            var aRow = new CSVRowData();
+            m_Rows.Add(aRow);
+            return aRow;
+        }
+        public string GetData(int iRow, int iColume) {
+            if(iRow < 0 || iRow >= m_Rows.Count) return string.Empty;
+            return m_Rows[iRow].Get(iColume);
+        }
+        public CSVRowData GetRow(int iRow) {
+            if(iRow < 0 || iRow >= m_Rows.Count) return null;
+            return m_Rows[iRow];
+        }
+        public List<CSVRowData> m_Rows = new List<CSVRowData>();
+    }
+}
---
+++ UCL_Core_Scripts/CsvCore/UCL_CSVParser.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace UCL.Core.CsvLib
+{
+    [UCL.Core.ATTR.EnableUCLEditor]
+    public class UCL_CSVParser : MonoBehaviour {
+
+        public string m_TextData = string.Empty;
+        public string m_FolderPath = string.Empty;
+        public string m_FileName = string.Empty;
+        public string m_SaveName = string.Empty;
+        public string m_Encoding = "big5";
+        public string FilePath { get { return Path.Combine(m_FolderPath, m_FileName); } }
+        public string SaveFilePath { get { return Path.Combine(m_FolderPath, m_SaveName); } }
+        public CSVData m_CSVData = null;
+#if UNITY_EDITOR
+        [UCL.Core.ATTR.UCL_FunctionButton]
+        public void ExploreFile() {
+            var path = UCL.Core.FileLib.EditorLib.OpenAssetsFileExplorer(m_FolderPath);
+            m_FolderPath = UCL.Core.FileLib.Lib.RemoveFolderPath(path, 1);
+            m_FileName = path.Substring(m_FolderPath.Length + 1, path.Length - m_FolderPath.Length - 1);
+        }
+#endif
+#if UNITY_STANDALONE_WIN
+        [UCL.Core.ATTR.UCL_FunctionButton]
+        public void OpenFile() {
+            var path = FilePath;
+            if(!File.Exists(path)) {
+                Debug.LogError("OpenFile:" + path + " fail, file not exist!!");
+                return;
+            }
+            UCL.Core.FileLib.WindowsLib.OpenAssetExplorer(path);
+        }
+#endif
+        [UCL.Core.ATTR.UCL_FunctionButton]
+        public void ReadFile() {
+            var path = FilePath;
+            if(!File.Exists(path)) {
+                Debug.LogError("ReadFile:" + path + " fail, file not exist!!");
+                return;
+            }
+            byte[] aBytes = File.ReadAllBytes(path);
+            //int i = 0;
+            //foreach(var e1 in Encoding.GetEncodings()) {
+            //    if(i++ > 100) break;
+            //    var aNewBytes = Encoding.Convert(e1.GetEncoding(), Encoding.UTF8, aBytes);
+            //    m_TextData = Encoding.UTF8.GetString(aNewBytes);
+            //    Debug.LogWarning("e1:" + e1.UCL_ToString() + ",m_TextData:" + m_TextData);
+            //}
+            if(!string.IsNullOrEmpty(m_Encoding)) {
+                aBytes = Encoding.Convert(Encoding.GetEncoding(m_Encoding), Encoding.UTF8, aBytes);
+            }
+            
+            m_TextData = Encoding.UTF8.GetString(aBytes);
+            m_CSVData = new CSVData(m_TextData);
+            Debug.LogWarning("m_CSVData:" + m_TextData);
+        }
+        public CSVData GetCSVData() {
+            return m_CSVData;
+        }
+        //public ClassData CreateClassData() {
+        //    return new ClassData(m_CSVData);
+        //}
+        //public void SaveClassToFile(ClassData iClassData) {
+        //    File.WriteAllText(SaveFilePath + ".cs", iClassData.ConvertToString());
+        //}
+        //[UCL.Core.ATTR.UCL_FunctionButton]
+        //public void WriteToClass() {
+        //    ClassData aClassData = CreateClassData();
+        //    Debug.LogError(aClassData.UCL_ToString());
+        //    SaveClassToFile(aClassData);
+        //    //File.WriteAllText(SaveFilePath + ".cs", aClassData.ConvertToString());
+        //}
+    }
+}

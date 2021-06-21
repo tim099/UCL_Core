3b5336b6e01ac41daf110aa3ea77aacdca3268c6
Tim Yeh <tim11251994@gmail.com>
2021-06-21 17:32:42 +0800

    Update UCL_TE_ColorReplace.cs(CodeStyle Optimize) Update UCL_TextureLib.cs(Add ResizeTexture(string iPath, int iMaxSize))

--- UCL_Core_Scripts/TextureCore/UCL_TextureEditors/UCL_TE_ColorReplace.cs
+++ UCL_Core_Scripts/TextureCore/UCL_TextureEditors/UCL_TE_ColorReplace.cs
@@ -9,30 +9,30 @@ namespace UCL.Core.TextureLib {
         public Color m_ReplaceColorMin;
         [Header("Max value of ReplaceColor")]
         public Color m_ReplaceColorMax;
-
+        [Header("Replace color between Max and Min to m_Color")]
         public Color m_Color;
 
         public bool m_GreyScaleAlpha = false;
         [ATTR.UCL_FunctionButton]
         public void ReplaceColor() {
-            int width = m_Texture.width;
-            int height = m_Texture.height;
-            var texture = new Texture2D(width, height, m_TextureFormat, false);
-            for(int y = 0; y < height; y++) {
-                for(int x = 0; x < width; x++) {
-                    var col = m_Texture.GetPixel(x,y);
+            int aWidth = m_Texture.width;
+            int aHeight = m_Texture.height;
+            var aTexture = new Texture2D(aWidth, aHeight, m_TextureFormat, false);
+            for(int y = 0; y < aHeight; y++) {
+                for(int x = 0; x < aWidth; x++) {
+                    var aCol = m_Texture.GetPixel(x, y);
                     if(m_GreyScaleAlpha) {
-                        col.a = (col.r + col.g + col.b) / 3f;
+                        aCol.a = (aCol.r + aCol.g + aCol.b) / 3f;
                     }
-                    if(col.IsBetween(m_ReplaceColorMin, m_ReplaceColorMax)) {
-                        col = m_Color;
+                    if(aCol.IsBetween(m_ReplaceColorMin, m_ReplaceColorMax)) {
+                        aCol = m_Color;
                     }
 
-                    texture.SetPixel(x, y, col);
+                    aTexture.SetPixel(x, y, aCol);
 
                 }
             }
-            SaveTexture(texture);
+            SaveTexture(aTexture);
         }
     }
 }
--- UCL_Core_Scripts/TextureCore/UCL_TextureLib.cs
+++ UCL_Core_Scripts/TextureCore/UCL_TextureLib.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 namespace UCL.Core.TextureLib {
     public enum ImageFormat {
@@ -30,22 +31,50 @@ namespace UCL.Core.TextureLib {
     }
 #endif
     static public class Lib {
-        public static void SaveTexture(string path, Texture2D texture, ImageFormat format) {
-            switch(format) {
+        /// <summary>
+        /// Save texture to file
+        /// </summary>
+        /// <param name="iPath"></param>
+        /// <param name="iTexture"></param>
+        public static void SaveTexture(string iPath, Texture2D iTexture)
+        {
+            string aPath = iPath.ToLower();
+            if (aPath.Contains(".png"))
+            {
+                SavePNG(iPath, iTexture);
+            }else if (aPath.Contains(".jpg"))
+            {
+                SaveJPG(iPath, iTexture);
+            }
+        }
+        public static void SaveTexture(string iPath, Texture2D iTexture, ImageFormat iFormat) {
+            switch(iFormat) {
                 case ImageFormat.png: 
                     {
-                        SavePNG(path, texture);
+                        SavePNG(iPath, iTexture);
                         break;
                     }
                 case ImageFormat.jpg: {
-                        SaveJPG(path, texture);
+                        SaveJPG(iPath, iTexture);
                         break;
                     }
             }
         }
-        public static void SavePNG(string path, Texture2D texture) {
-            Core.FileLib.Lib.CreateDirectory(Core.FileLib.Lib.GetFolderPath(path));
-            System.IO.File.WriteAllBytes(path + ".png", texture.EncodeToPNG());
+        public static void SavePNG(string iPath, Texture2D texture) {
+            Core.FileLib.Lib.CreateDirectory(Core.FileLib.Lib.GetFolderPath(iPath));
+            if (iPath.Contains(".PNG"))
+            {
+                iPath = iPath.Replace(".PNG", ".png");
+            }
+            else
+            {
+                if (!iPath.Contains(".png"))
+                {
+                    iPath = iPath + ".png";
+                }
+            }
+
+            System.IO.File.WriteAllBytes(iPath, texture.EncodeToPNG());
 #if UNITY_EDITOR
             UCL.Core.EditorLib.AssetDatabaseMapper.Refresh();
 #endif
@@ -56,6 +85,61 @@ namespace UCL.Core.TextureLib {
 #if UNITY_EDITOR
             UCL.Core.EditorLib.AssetDatabaseMapper.Refresh();
 #endif
+        }
+
+        /// <summary>
+        /// Resize the target Texture if the texture bigger than iMaxSize
+        /// </summary>
+        /// <param name="iPath"></param>
+        /// <param name="iMaxSize"></param>
+        public static void ResizeTexture(string iPath, int iMaxSize)
+        {
+            if (!File.Exists(iPath))
+            {
+                Debug.LogError("ResizeTexture fail,iPath:" + iPath + ",!File.Exists");
+                return;
+            }
+            try
+            {
+                var aFileData = File.ReadAllBytes(iPath);
+                var aTex = new Texture2D(2, 2);
+                aTex.LoadImage(aFileData); //..this will auto-resize the texture dimensions.
+
+                bool aIsResized = false;
+                int aWidth = aTex.width;
+                int aHeight = aTex.height;
+                //Debug.LogWarning("aIconPath:" + iPath + "Width:" + aWidth + ",Height:" + aHeight);
+                if (aTex.width > aTex.height)
+                {
+                    if (aTex.width > iMaxSize)
+                    {
+                        float aScale = (float)iMaxSize / aTex.width;
+                        aTex.ResizeTexture(iMaxSize, Mathf.RoundToInt(aTex.height * aScale));
+                        aIsResized = true;
+                    }
+                }
+                else
+                {
+                    if (aTex.height > iMaxSize)
+                    {
+                        float aScale = (float)iMaxSize / aTex.height;
+                        aTex.ResizeTexture(Mathf.RoundToInt(aTex.width * aScale), iMaxSize);
+                        aIsResized = true;
+                    }
+                }
+                if (aIsResized)
+                {
+                    Debug.Log("Resize Texture:" + iPath + ",Origin Width:" + aWidth + ",Origin Height:" + aHeight
+                        + ",new Width:" + aTex.width + ",new Height:" + aTex.height);
+                    //SavePNG(iPath, aTex);
+                    SaveTexture(iPath, aTex);
+                }
+                UnityEngine.Object.DestroyImmediate(aTex);
+            }
+            catch (System.Exception iE)
+            {
+                Debug.LogException(iE);
+            }
         }
         /// <summary>
         /// Scale the target texture to target size!!

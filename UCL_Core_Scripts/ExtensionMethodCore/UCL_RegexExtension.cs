abe89ad03129865a75a14f7dc5a316813dab2f2a
Tim Yeh <tim11251994@gmail.com>
2021-10-18 17:35:47 +0800

    Add UCL_RegexExtension.cs

---
+++ UCL_Core_Scripts/ExtensionMethodCore/UCL_RegexExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class RegexExtensionMethods
+{
+    public static string HightLight(this System.Text.RegularExpressions.Regex iRegex, string iDisplayName, string iSearchName)
+    {
+        if (iRegex == null) return iDisplayName;
+        if (iRegex != null)
+        {
+            var aMaches = iRegex.Matches(iDisplayName.ToLower());
+            if (aMaches.Count > 0)
+            {
+                var aMach = aMaches[0];//aRegex.Matches(aDisplayName);
+                if (aMach.Success)
+                {
+                    int aMatchAt = aMach.Index;
+                    string aValue = iDisplayName.Substring(aMatchAt, iSearchName.Length);
+                    iDisplayName = iDisplayName.Substring(0, aMatchAt)
+                        + aValue.RichTextColor(Color.red)
+                        + iDisplayName.Substring(aMatchAt + aValue.Length, iDisplayName.Length - aValue.Length - aMatchAt);
+                }
+            }
+        }
+
+        return iDisplayName;
+    }
+}

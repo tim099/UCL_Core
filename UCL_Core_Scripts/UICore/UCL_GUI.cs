dd641299012b3ded56ede87fddb85b6a9666fe3d
Tim Yeh <tim11251994@gmail.com>
2021-12-17 17:34:20 +0800

    Update UCL_GUI.cs

--- UCL_Core_Scripts/UICore/UCL_GUI.cs
+++ UCL_Core_Scripts/UICore/UCL_GUI.cs
@@ -3,6 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace UCL.Core.UI {
+    public class DraggableButton
+    {
+        public string Name { get; set; }
+        Rect mButtonRect = new Rect(10, 10, 200, 100);
+        Vector2 mMouseToPositionDelta = Vector2.zero;
+        bool mButtonPressed = false;
+        bool mDraged = false;
+        public bool OnGUI()
+        {
+            var aCurrentEvent = Event.current;
+            var aMousePos = aCurrentEvent.mousePosition;
+            if (mButtonRect.Contains(Event.current.mousePosition))
+            {
+                if (Event.current.type == EventType.MouseDown)
+                {
+                    mMouseToPositionDelta = mButtonRect.position - aMousePos;
+                    mButtonPressed = true;
+                }
+                else if (Event.current.type == EventType.MouseUp)
+                {
+                    mButtonPressed = false;
+                }
+                
+            }
+            mDraged = false;
+            if (mButtonPressed && Event.current.type == EventType.MouseDrag)
+            {
+                mButtonRect.position = mMouseToPositionDelta + aMousePos;
+                mDraged = true;
+            }
+            bool aIsClicked = false;
+            if (GUI.Button(mButtonRect, Name))
+            {
+                if (!mDraged)
+                {
+                    aIsClicked = true;
+                }
+            }
+            return aIsClicked;
+        }
+    }
     public static class UCL_GUI{
         static Stack<Color> m_BackGroundColorStack = new Stack<Color>();
         static Stack<System.Action> m_UndoStack = new Stack<System.Action>();

0d6ba1de925e245e10b3636bed2ffb0f1f000032
Tim Yeh <tim11251994@gmail.com>
2021-12-30 17:35:22 +0800

    Add UCL_EnumExtension.cs

---
+++ UCL_Core_Scripts/ExtensionMethodCore/UCL_EnumExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL.Core
+{
+    public static partial class UCL_EnumExtension
+    {
+        /// <summary>
+        /// return an array contains all enum value of iEnum
+        /// </summary>
+        /// <param name="iEnum"></param>
+        /// <returns></returns>
+        public static Array GetValues(this Enum iEnum)
+        {
+            if (iEnum == null) return null;
+            return Enum.GetValues(iEnum.GetType());
+        }
+    }
+}
+

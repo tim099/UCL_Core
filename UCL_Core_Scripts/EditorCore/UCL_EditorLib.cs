337f5ee3df8374502870eef52e32e458fb74a405
Tim Yeh <tim11251994@gmail.com>
2022-04-11 17:35:30 +0800

    Update UCL_Core_Scripts/EditorCore/UCL_EditorLib.cs

---
+++ UCL_Core_Scripts/EditorCore/UCL_EditorLib.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UCL.Core.EditorLib
+{
+    public static class UCL_EditorLib
+    {
+        /// <summary>
+        /// Removes all MonoBehaviours with a missing script from the given Transform and all childs.
+        /// </summary>
+        /// <param name="iTarget"></param>
+        static public void RemoveMonoBehavioursWithMissingScript(Transform iTarget)
+        {
+            UnityEditor.GameObjectUtility.RemoveMonoBehavioursWithMissingScript(iTarget.gameObject);
+            foreach (Transform aChild in iTarget)
+            {
+                RemoveMonoBehavioursWithMissingScript(aChild);
+            }
+        }
+    }
+}

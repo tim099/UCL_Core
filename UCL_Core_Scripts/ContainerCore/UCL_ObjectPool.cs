770ca8d20e3796f75a6a23cf86015add40157763
Tim <tim11251994@gmail.com>
2023-09-15 18:34:16 +0800

    Add UnityGameObjectPool

--- UCL_Core_Scripts/ContainerCore/UCL_ObjectPool.cs
+++ UCL_Core_Scripts/ContainerCore/UCL_ObjectPool.cs
@@ -56,6 +56,7 @@ namespace UCL.Core.Container {
         public T m_Template = null;
         public void Prewarm(int iCount)
         {
+            if (iCount <= 0) return;
             Transform aParent = null;
             if (m_DeleteRoot != null)
             {
@@ -93,7 +94,7 @@ namespace UCL.Core.Container {
                     m_InitAction.Invoke(aTarget);
                 }
                 
-                aObj.gameObject.SetActive(false);
+                aObj.SetActive(false);
                 m_ObjPool.Push(aTarget);
             }
 
@@ -171,6 +172,166 @@ namespace UCL.Core.Container {
                     }
                     m_ObjPool.Push(aTarget);
                     aTarget.gameObject.SetActive(false);
+                }
+            }
+            m_AllObjs.Clear();
+        }
+    }
+
+    public class UnityGameObjectPool
+    {
+        public UnityGameObjectPool() { }
+        public UnityGameObjectPool(GameObject iTemplate, System.Action<GameObject> iInitAction = null, System.Func<GameObject, Transform, GameObject> iCreateAction = null)
+        {
+            m_Template = iTemplate;
+            m_InitAction = iInitAction;
+            m_CreateAction = iCreateAction;
+        }
+        /// <summary>
+        /// call when object first time created
+        /// </summary>
+        public System.Action<GameObject> m_InitAction = null;
+        /// <summary>
+        /// if not null, will create object using this instead of GameObject.Instantiate(m_Template, iParent)
+        /// </summary>
+        public System.Func<GameObject, Transform, GameObject> m_CreateAction = null;
+        public string m_CreateName = "GameObject";
+        /// <summary>
+        /// if not null, deleted object will set this as parent
+        /// </summary>
+        public Transform m_DeleteRoot = null;
+
+        Stack<GameObject> m_ObjPool = new Stack<GameObject>();
+        /// <summary>
+        /// All Object that Created
+        /// </summary>
+        List<GameObject> m_AllObjs = new List<GameObject>();
+        /// <summary>
+        /// Template of created object
+        /// (if not null, will use Instantiate(m_Template, iParent) to create GameObject
+        /// </summary>
+        public GameObject m_Template = null;
+        public void Prewarm(int iCount)
+        {
+            if (iCount <= 0) return;
+
+            Transform aParent = null;
+            if (m_DeleteRoot != null)
+            {
+                aParent = m_DeleteRoot;
+            }
+
+            for (int i = 0; i < iCount; i++)
+            {
+                GameObject aObj = null;
+                if (m_Template != null)
+                {
+                    if (m_CreateAction != null)
+                    {
+                        aObj = m_CreateAction(m_Template, aParent);
+                    }
+                    else
+                    {
+                        aObj = GameObject.Instantiate(m_Template, aParent);
+                    }
+                }
+                else
+                {
+                    aObj = new GameObject(m_CreateName);
+                    aObj.transform.SetParent(aParent);
+                }
+
+                if (aParent)
+                {
+                    aObj.layer = aParent.gameObject.layer;
+                }
+
+                if (m_InitAction != null)
+                {
+                    m_InitAction.Invoke(aObj);
+                }
+
+                aObj.SetActive(false);
+                m_ObjPool.Push(aObj);
+            }
+
+        }
+        /// <summary>
+        /// Create a object from pool
+        /// </summary>
+        /// <param name="iParent"></param>
+        /// <returns></returns>
+        public GameObject Create(Transform iParent = null)
+        {
+            GameObject aTarget = null;
+            if (m_ObjPool.Count > 0)
+            {
+                aTarget = m_ObjPool.Pop();
+            }
+            else
+            {
+                if (m_Template != null)
+                {
+                    if (m_CreateAction != null)
+                    {
+                        aTarget = m_CreateAction(m_Template, iParent);
+                    }
+                    else
+                    {
+                        aTarget = GameObject.Instantiate(m_Template, iParent);
+                    }
+                }
+                else
+                {
+                    aTarget = new GameObject(m_CreateName);
+                }
+                if (iParent)
+                {
+                    aTarget.layer = iParent.gameObject.layer;
+                }
+
+                if (m_InitAction != null)
+                {
+                    m_InitAction.Invoke(aTarget);
+                }
+            }
+            aTarget.SetActive(true);
+            if (iParent != aTarget.transform.parent) aTarget.transform.SetParent(iParent);
+
+            m_AllObjs.Add(aTarget);
+            return aTarget;
+        }
+        /// <summary>
+        /// delete object(return to pool so object will be reusable
+        /// </summary>
+        /// <param name="iTarget"></param>
+        public void Delete(GameObject iTarget)
+        {
+
+            iTarget.SetActive(false);
+            if (m_DeleteRoot != null)
+            {
+                iTarget.transform.SetParent(m_DeleteRoot);
+            }
+            m_ObjPool.Push(iTarget);
+            m_AllObjs.Remove(iTarget);
+        }
+        /// <summary>
+        /// delete all object created from pool
+        /// </summary>
+        public void DeleteAll()
+        {
+            for (int i = 0; i < m_AllObjs.Count; i++)
+            {
+                var aTarget = m_AllObjs[i];
+                if (aTarget != null)
+                {
+                    if (m_DeleteRoot != null)
+                    {
+                        aTarget.transform.SetParent(m_DeleteRoot);
+                    }
+                    m_ObjPool.Push(aTarget);
+                    aTarget.SetActive(false);
                 }
             }
             m_AllObjs.Clear();

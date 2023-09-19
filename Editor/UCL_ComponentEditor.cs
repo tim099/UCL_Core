c53129ffa20d3f232f675a2f367f1fd15e241bed
Tim <tim11251994@gmail.com>
2023-09-19 18:33:24 +0800

    Update UCL_ComponentEditor

--- Editor/UCL_ComponentEditor.cs
+++ Editor/UCL_ComponentEditor.cs
@@ -1,37 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 namespace UCL.Core {
 #if UNITY_EDITOR
-    static public class UCL_ComponentEditor{
-        static SerializedObject m_Src;
-        [MenuItem("CONTEXT/Component/UCL_Copy value")]
-        public static void CopySerialized(MenuCommand cmd) { m_Src = new SerializedObject(cmd.context); }
+    static public class UCL_ComponentEditor
+    {
+        static UnityEditor.SerializedObject s_Src;
+        [UnityEditor.MenuItem("CONTEXT/Component/UCL/Copy value")]
+        public static void CopySerialized(UnityEditor.MenuCommand iCmd) { s_Src = new UnityEditor.SerializedObject(iCmd.context); }
 
-        [MenuItem("CONTEXT/Component/UCL_Paste value")]
-        public static void PasteSerialized(MenuCommand cmd) {
-            if(m_Src == null) return;
-            var SrcType = m_Src.targetObject.GetType();
-            var DstType = cmd.context.GetType();
-            if(SrcType == DstType) {
-                Debug.LogWarning("PasteSerialized Same Type:" + SrcType.ToString());
-                EditorUtility.CopySerialized(m_Src.targetObject, cmd.context);
+        [UnityEditor.MenuItem("CONTEXT/Component/UCL/Paste value")]
+        public static void PasteSerialized(UnityEditor.MenuCommand iCmd)
+        {
+            if (s_Src == null) return;
+            var aSrcType = s_Src.targetObject.GetType();
+            var aDstType = iCmd.context.GetType();
+            if (aSrcType == aDstType)
+            {
+                //Debug.LogWarning("PasteSerialized Same Type:" + aSrcType.ToString());
+                UnityEditor.EditorUtility.CopySerialized(s_Src.targetObject, iCmd.context);
                 return;
             }
-            Debug.LogWarning("PasteSerialized Type Src:" + SrcType.ToString() + ",Dst:" + DstType);
-            SerializedObject dst = new SerializedObject(cmd.context);
-            SerializedProperty itr = m_Src.GetIterator();
-            if(itr.NextVisible(true)) {
-                while(itr.NextVisible(true)) {
-                    SerializedProperty prop_element = dst.FindProperty(itr.name);
-                    if(prop_element != null && prop_element.propertyType == itr.propertyType) {
-                        dst.CopyFromSerializedProperty(itr);
+            //Debug.LogWarning("PasteSerialized Type Src:" + aSrcType.ToString() + ",Dst:" + aDstType);
+            UnityEditor.SerializedObject aDst = new UnityEditor.SerializedObject(iCmd.context);
+            UnityEditor.SerializedProperty aItr = s_Src.GetIterator();
+            if (aItr.NextVisible(true))
+            {
+                while (aItr.NextVisible(true))
+                {
+                    UnityEditor.SerializedProperty aSerializedProperty = aDst.FindProperty(aItr.name);
+                    if (aSerializedProperty != null && aSerializedProperty.propertyType == aItr.propertyType)
+                    {
+                        aDst.CopyFromSerializedProperty(aItr);
                     }
                 }
             }
-            dst.ApplyModifiedProperties();
+            aDst.ApplyModifiedProperties();
         }
     }
 #endif

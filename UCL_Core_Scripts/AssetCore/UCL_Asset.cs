a3b7b9e7e26a2d03216ef8e5a0be6fc440b00901
Tim <tim11251994@gmail.com>
2023-10-25 18:34:28 +0800

    add UCL_Asset

---
+++ UCL_Core_Scripts/AssetCore/UCL_Asset.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UCL.Core.JsonLib;
+using UnityEngine;
+
+namespace UCL.Core
+{
+    public interface UCLI_ID
+    {
+        string ID { get; set; }
+    }
+    public class UCL_Asset<T> : UnityJsonSerializable, UCLI_ID where T : class, new()
+    {
+        #region static
+        /// <summary>
+        /// Util object for this asset
+        /// </summary>
+        public static T Util
+        {
+            get
+            {
+                if(s_Util == null)
+                {
+                    s_Util = new T();
+                }
+                return s_Util;
+            }
+        }
+        private static T s_Util = null;
+        #endregion
+
+        #region Interface
+        public string ID { get => m_ID; set => m_ID = value; }
+        #endregion
+
+
+        [SerializeField]
+        protected string m_ID;
+    }
+}
+

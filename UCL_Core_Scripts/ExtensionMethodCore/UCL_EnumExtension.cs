54ed3afab709119fa34c5bb80a2f0b69e3849b0e
Tim <tim11251994@gmail.com>
2024-04-02 07:45:10 +0800

    UCL_EnumExtension add NextValue, PrevValue

--- UCL_Core_Scripts/ExtensionMethodCore/UCL_EnumExtension.cs
+++ UCL_Core_Scripts/ExtensionMethodCore/UCL_EnumExtension.cs
@@ -17,6 +17,62 @@ namespace UCL.Core
             if (iEnum == null) return null;
             return Enum.GetValues(iEnum.GetType());
         }
+        /// <summary>
+        /// Get next enum value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="iValue"></param>
+        /// <returns></returns>
+        public static T NextValue<T>(this T iValue) where T : System.Enum
+        {
+            try
+            {
+                var aValues = Enum.GetValues(iValue.GetType());
+                int aIndex = System.Array.IndexOf(aValues, iValue);
+                if (aIndex >= aValues.Length - 1)//last element
+                {
+                    return (T)aValues.GetValue(0);//first element
+                }
+                else
+                {
+                    return (T)aValues.GetValue(aIndex + 1);
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogException(e);
+            }
+
+            return iValue;
+        }
+        /// <summary>
+        /// Get prev enum value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="iValue"></param>
+        /// <returns></returns>
+        public static T PrevValue<T>(this T iValue) where T : System.Enum
+        {
+            try
+            {
+                var aValues = Enum.GetValues(iValue.GetType());
+                int aIndex = System.Array.IndexOf(aValues, iValue);//first element
+                if (aIndex <= 0)
+                {
+                    return (T)aValues.GetValue(aValues.Length - 1);//last element
+                }
+                else
+                {
+                    return (T)aValues.GetValue(aIndex - 1);
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogException(e);
+            }
+
+            return iValue;
+        }
     }
 }
 

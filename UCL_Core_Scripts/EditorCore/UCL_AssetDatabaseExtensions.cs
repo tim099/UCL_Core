7c69e42074b3df74d9ed3f43156d34e504cfd3ad
Tim <tim11251994@gmail.com>
2025-06-27 00:36:39 +0800

    add UCL_AssetDatabaseExtensions

---
+++ UCL_Core_Scripts/EditorCore/UCL_AssetDatabaseExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+// ATS_AutoHeader
+// to change the auto header please go to ATS_AutoHeader.cs
+using UnityEngine;
+
+namespace UCL.Core
+{
+    public class UCL_AssetDatabaseExtensions
+    {
+        public static void CreateOrUpdateScriptableObject<T>(string assetPath, System.Action<T> setAsset) where T : ScriptableObject
+        {
+#if UNITY_EDITOR
+            var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            if (asset != null)
+            {
+                setAsset(asset);
+                UnityEditor.EditorUtility.SetDirty(asset);
+                UnityEditor.AssetDatabase.SaveAssetIfDirty(asset);
+            }
+            else
+            {
+                asset = ScriptableObject.CreateInstance<T>();
+                setAsset(asset);
+                UnityEditor.AssetDatabase.CreateAsset(asset, assetPath);
+            }
+#endif
+        }
+    }
+}
